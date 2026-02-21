9626d4cb55dd52b3b4d34fb7847977e195a87b39
mzk <mzk159753@gmail.com>
2026-02-21 23:27:20 +0900

    Level 3: Add stock list display feature

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("JavaScript connected");
+let totalValue = 0;
+
+let stockList = document.getElementById("stockList")
+
+let addButton = document.getElementById("addButton");
+
+addButton.onclick = function() {
+
+    let stockName = document.getElementById("stockName").value;
+    let shares = document.getElementById("shares").value;
+    let price = document.getElementById("price").value;
+
+    let value = shares * price;
+
+    totalValue = totalValue + value;
+
+    let listItem = document.createElement("li");
+
+    listItem.innerText = stockName + " - " + shares + " shares - $" + price;
+
+    stockList.appendChild(listItem);
+
+    document.getElementById("totalValue").innerText =
+        "Total Value: $" + totalValue;
+}
