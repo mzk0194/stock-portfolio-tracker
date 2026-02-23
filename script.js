fe10b6b13a657229fa265733371c7d75904b5071
mz k <mzk159753@gmail.com>
2026-02-23 16:43:34 +0900

    Level4, 5: Add delete functionality and render structure

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
-let totalValue = 0;
+let stocks = [];
 
-let stockList = document.getElementById("stockList")
-
+let stockList = document.getElementById("stockList");
 let addButton = document.getElementById("addButton");
+
+function renderStocks() {
+
+    stockList.innerHTML = "";
+
+    let total = 0;
+
+    stocks.forEach(function(stock, index) {
+
+        total += stock.shares * stock.price;
+
+        let listItem = document.createElement("li");
+        listItem.innerText =
+            stock.name + " - " +
+            stock.shares + " shares - $" +
+            stock.price + " ";
+
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+
+        deleteButton.onclick = function() {
+            stocks.splice(index, 1);
+            renderStocks();
+        };
+
+        listItem.appendChild(deleteButton);
+        stockList.appendChild(listItem);
+    });
+
+    document.getElementById("totalValue").textContent =
+        "Total Value: $" + total;
+}
 
 addButton.onclick = function() {
 
     let stockName = document.getElementById("stockName").value;
-    let shares = document.getElementById("shares").value;
-    let price = document.getElementById("price").value;
+    let shares = parseFloat(document.getElementById("shares").value);
+    let price = parseFloat(document.getElementById("price").value);
 
-    let value = shares * price;
+    if (!stockName || isNaN(shares) || isNaN(price)) {
+        alert("Please enter valid stock name and numbers");
+        return;
+    }
 
-    totalValue = totalValue + value;
+    stocks.push({
+        name: stockName,
+        shares: shares,
+        price: price
+    });
 
-    let listItem = document.createElement("li");
+    renderStocks();
 
-    listItem.innerText = stockName + " - " + shares + " shares - $" + price;
-
-    stockList.appendChild(listItem);
-
-    document.getElementById("totalValue").innerText =
-        "Total Value: $" + totalValue;
-}
+    document.getElementById("stockName").value = "";
+    document.getElementById("shares").value = "";
+    document.getElementById("price").value = "";
+};
