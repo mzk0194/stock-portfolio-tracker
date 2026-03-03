f24b5e9ee66d25f04f71dc410af8855b4735d967
mz k <mzk159753@gmail.com>
2026-03-03 22:09:13 +0900

    Level 8: Add automatic price update with setIntervalo and API error handling

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let stockList = document.getElementById("stockList");
 let addButton = document.getElementById("addButton");
 
 function renderStocks() {
-
     stockList.innerHTML = "";
 
     let total = 0;
@@ -33,6 +32,8 @@ function renderStocks() {
 
     document.getElementById("totalValue").textContent =
         "Total Value: $" + total;
+
+    localStorage.setItem("stocks", JSON.stringify(stocks));
 }
 
 addButton.onclick = function() {
@@ -57,4 +58,85 @@ addButton.onclick = function() {
     document.getElementById("stockName").value = "";
     document.getElementById("shares").value = "";
     document.getElementById("price").value = "";
+
 };
+
+let savedStocks = localStorage.getItem("stocks");
+
+if (savedStocks) {
+    stocks = JSON.parse(savedStocks);
+    renderStocks();
+}
+
+let getPriceButton = document.getElementById("getPriceButton");
+
+getPriceButton.onclick = async function () {
+
+    let stockName = document.getElementById("stockName").value;
+
+    if (!stockName) {
+        alert("Please enter stock symbol (e.g., AAPL)");
+        return;
+    }
+
+    try {
+
+        let response = await fetch(
+            `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockName}&apikey=2HXV9OJ3RF4F34VV`
+        );
+
+        let data = await response.json();
+
+        if (data["Global Quote"] && data["Global Quote"]["05. price"]) {
+
+            let price = data["Global Quote"]["05. price"];
+            document.getElementById("price").value =
+                parseFloat(price).toFixed(2);
+
+        } else {
+            console.log(data); 
+            alert("API limit reached or invalid symbol.");
+        }
+
+    } catch (error) {
+
+        console.error("Error:", error);
+        alert("Failed to fetch stock price.");
+
+    }
+
+};
+
+async function updatePrices() {
+
+    console.log("업데이트 실행됨");
+
+    for (let stock of stocks) {
+
+        try {
+
+            let response = await fetch(
+                `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock.name}&apikey=2HXV9OJ3RF4F34VV`
+            );
+
+            let data = await response.json();
+
+            if (data["Global Quote"] && data["Global Quote"]["05. price"]) {
+
+                let newPrice = data["Global Quote"]["05. price"];
+                stock.price = parseFloat(newPrice);
+
+            } else {
+                console.log("API limit reached or invalid symbol");
+            }
+
+        } catch (error) {
+            console.error("Update error:", error);
+        }
+
+    }
+
+    renderStocks();
+}
+
+//setInterval(updatePrices, 60000);
